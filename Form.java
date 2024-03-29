f4a28def29bc2d904d06a04340aaa6277ac2c74f
Abdul Wahab Mufti <awmufti3@gmail.com>
2024-03-28 23:05:51 -0400

    init

---
+++ Form.java
@@ -0,0 +1,17 @@
+public class Form
+{
+    private String requestersName;
+
+    private String mailingAddress;
+
+    private String email;
+
+    private String requestedDoc;
+
+    private String deceasedImmigrantName;
+
+    private String deceasedImmigrantDOB;
+
+    private String deceasedImmigrantPOB;
+    
+}
